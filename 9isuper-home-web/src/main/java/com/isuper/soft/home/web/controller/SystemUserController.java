0f2f6721a65181d319dc243aefc22341f60cc20e
邹永乐 <yongle.zou@bl.com>
2017-08-17 17:12:59 +0800

    commit code

--- 9isuper-home-web/src/main/java/com/isuper/soft/home/web/controller/SystemUserController.java
+++ 9isuper-home-web/src/main/java/com/isuper/soft/home/web/controller/SystemUserController.java
@@ -1,9 +1,14 @@
 package com.isuper.soft.home.web.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,10 +26,41 @@ public class SystemUserController extends BaseController {
 	@Inject
 	private SystemUserService systemUserService;
 
+	@PreAuthorize("hasAnyAuthority('ROLE_SYSTEM_USER_LIST')")
 	@RequestMapping(value = { "", "list" })
 	@ResponseBody
-	private ModelAndView toList(Model model) {
+	public ModelAndView toList(Model model) {
 		model.addAttribute("allSystemUsers", this.systemUserService.findAllUser());
 		return new ModelAndView("system/userlist");
+	}
+
+	@PreAuthorize("hasAnyAuthority('ROLE_SYSTEM_USER_DEL')")
+	@RequestMapping(value = "/delete")
+	@ResponseBody
+	public Map<String, Object> deleteSystemUser(HttpServletRequest request) {
+		Map<String, Object> result = new HashMap<>();
+		try {
+			result.put("status", true);
+			result.put("message", "删除成功");
+		} catch (Exception e) {
+			result.put("status", false);
+			result.put("message", "删除异常");
+		}
+		return result;
+	}
+	
+	@PreAuthorize("hasAnyAuthority('ROLE_SYSTEM_USER_EDIT')")
+	@RequestMapping(value = "/edit")
+	@ResponseBody
+	public Map<String, Object> editSystemUser(HttpServletRequest request) {
+		Map<String, Object> result = new HashMap<>();
+		try {
+			result.put("status", true);
+			result.put("message", "删除成功");
+		} catch (Exception e) {
+			result.put("status", false);
+			result.put("message", "删除异常");
+		}
+		return result;
 	}
 }
